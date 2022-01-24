18bd65a6841f28ef25c6b6336efb6b54270cdb3e
n9ygd <58976884+n9ygd@users.noreply.github.com>
2022-01-25 01:10:03 +0800

    pull commit

--- src/com/ygd/Hello.java
+++ src/com/ygd/Hello.java
@@ -10,5 +10,6 @@ public class Hello {
         System.out.println("fuck 老孟");
         System.out.println("hot-fix add");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
