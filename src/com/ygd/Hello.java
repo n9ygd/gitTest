51f61824e2a857cf20c7a72ba4471c77b589752a
ygd <n9ygd@qq.com>
2022-01-25 00:05:02 +0800

    first commit

---
+++ src/com/ygd/Hello.java
@@ -0,0 +1,9 @@
+package com.ygd;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello ygd");
+    }
+}
