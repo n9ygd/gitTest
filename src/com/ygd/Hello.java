aa159c13a76e888fdcd9e631970803f6898ec037
ygd <n9ygd@qq.com>
2022-01-25 00:47:34 +0800

    master conflict commit

--- src/com/ygd/Hello.java
+++ src/com/ygd/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello ygd");
+        System.out.println("master conflicts");
         System.out.println("fuck 老孟");
         System.out.println("hot-fix add");
     }
